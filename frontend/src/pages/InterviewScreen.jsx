353d39d38734543b09d5a11eff89cd14107cf590
santhiya86 <23csea27santhiya@gmail.com>
2026-07-11 22:55:25 +0530

    Update InterviewScreen.jsx

--- frontend/src/pages/InterviewScreen.jsx
+++ frontend/src/pages/InterviewScreen.jsx
@@ -234,27 +234,23 @@ export default function InterviewScreen({ config, interviewId, onComplete, onExi
 
     const recognition = new SpeechRecognition();
     recognition.continuous      = true;
-    recognition.interimResults = false;
+    recognition.interimResults  = true;
     recognition.lang            = "en-US";
     recognition.maxAlternatives = 1;
 
-recognition.onresult = (event) => {
-  let finalTranscript = "";
-  let interimTranscript = "";
+    recognition.onstart = () => setRecording(true);
 
-  for (let i = 0; i < event.results.length; i++) {
-    const transcript = event.results[i][0].transcript;
-
-    if (event.results[i].isFinal) {
-      finalTranscript += transcript + " ";
-    } else {
-      interimTranscript += transcript;
-    }
-  }
-
-  finalTranscriptRef.current = finalTranscript.trim();
-  setLiveText((finalTranscript + interimTranscript).trim());
-};
+    recognition.onresult = (event) => {
+      let interim   = "";
+      let finalPart = "";
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const t = event.results[i][0].transcript;
+        if (event.results[i].isFinal) finalPart += t + " ";
+        else interim += t;
+      }
+      if (finalPart) finalTranscriptRef.current += finalPart;
+      setLiveText((finalTranscriptRef.current + interim).trim());
+    };
 
     recognition.onerror = (event) => {
       console.error("Speech recognition error:", event.error);
